1646293cda6d1ea25b2ecfe3f9f6688d338fb080
leonard-1234 <leonardnjh-pm23@student.tarc.edu.my>
2025-12-06 12:44:11 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.crqsmwykih0c.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee_db"
+custombucket = "leonardng-bucket"
+customregion = "us-east-1"
+
